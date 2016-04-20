93fb96a5738a2f3e0b3496d9a4b9751618d02430
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-20 12:56:11 +0100

    Improve bootstrap and the main compose file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
+#!/bin/sh -x
+
+test ! -f .bootstrap_done || exit
+
 for repo in hello monsterz-den ; do
   git clone -o upstream "git@github.com:ThePixelMonsterzApp/${repo}"
 done
+
+docker-compose -f scope/docker-compose.yml up -d
+
+touch .bootstrap_done
